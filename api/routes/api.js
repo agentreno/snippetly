079b8e1b057c71f334a163225dad1aa4f7072a49
Karl <karl.turrell@buto.tv>
2018-03-24 11:13:09 +0000

    Add snippets dummy data, schema and resolver combined

--- api/routes/api.js
+++ api/routes/api.js
@@ -1,18 +1,52 @@
 var express = require('express')
 var graphqlHTTP = require('express-graphql')
-var { buildSchema } = require('graphql')
+var { makeExecutableSchema } = require('graphql-tools')
 
-var schema = buildSchema(`
-    type Query {
-        hello: String
+const typeDefs = `
+    type Snippet {
+        id: Int!
+        language: String
+        title: String
+        body: String
     }
-`)
 
-var root = { hello: () => 'Hello world!' }
+    type Query {
+        snippets: [Snippet]
+    }
+`
+
+// Dummy data
+const snippets = [
+    {
+        id: 1,
+        language: 'Javascript',
+        title: 'Hello World',
+        body: 'console.log("hello world")'
+    },
+    {
+        id: 2,
+        language: 'Python',
+        title: 'Hello World',
+        body: 'print("hello world")'
+    },
+]
+
+// Snippet resolver
+const resolvers = {
+    Query: {
+        snippets: () => snippets,
+    },
+}
+
+
+// Combine schema and resolvers
+var executableSchema = makeExecutableSchema({
+    typeDefs,
+    resolvers,
+})
 
 var routeHandler = graphqlHTTP({
-    schema: schema,
-    rootValue: root,
+    schema: executableSchema,
     graphiql: true,
 })
 
