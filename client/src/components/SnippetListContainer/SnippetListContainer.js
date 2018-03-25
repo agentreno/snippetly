9d2e0a515e79d3aa4563f9bc55c2cf1e5060babe
Karl <karl.turrell@buto.tv>
2018-03-25 18:33:07 +0100

    Swap Redux state management for Apollo
    Apollo has a Provider and Query component that wraps presentation components
    providing error, loading and data from API interactions. Very simple... but
    it seems like magic at the moment, and that makes me suspicious...

--- client/src/components/SnippetListContainer/SnippetListContainer.js
+++ client/src/components/SnippetListContainer/SnippetListContainer.js
@@ -1,15 +1,31 @@
-import { connect } from 'react-redux'
+import React from 'react'
+import { Query } from 'react-apollo'
+import gql from 'graphql-tag'
 
 import SnippetList from '../SnippetList/SnippetList'
 
-const mapStateToProps = state => ({
-    snippets: state.snippets
-})
+const snippetsQuery = gql`
+    {
+        snippets {
+            _id,
+            language,
+            title,
+            body
+        }
+    }
+`
 
-// No mapDispatchToProps yet, no actions fired from UI yet
+const SnippetListContainer = () => (
+    <Query query={snippetsQuery}>
+        {
+            ({ loading, error, data }) => {
+                if (loading) return <p>Loading...</p>
+                if (error) return <p>Error</p>
 
-const SnippetListContainer = connect(
-    mapStateToProps
-)(SnippetList)
+                return <SnippetList snippets={data.snippets} />
+            }
+        }
+    </Query>
+)
 
 export default SnippetListContainer
--- client/src/index.js
+++ client/src/index.js
@@ -1,31 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { createStore, applyMiddleware } from 'redux'
-import { Provider } from 'react-redux'
-import { createLogger } from 'redux-logger'
-import thunkMiddleware from 'redux-thunk'
+import ApolloClient from 'apollo-boost'
+import { ApolloProvider } from 'react-apollo'
 
 import registerServiceWorker from './registerServiceWorker'
 import './index.css'
 import App from './components/App/App'
-import rootReducer from './reducers'
-import { fetchSnippets } from './actions'
 
-// Create store and fetch data immediately
-const loggerMiddleware = createLogger()
-let store = createStore(
-    rootReducer,
-    applyMiddleware(
-        loggerMiddleware,
-        thunkMiddleware
-    )
-)
-store.dispatch(fetchSnippets())
+// Apollo client
+const client = new ApolloClient({
+    uri: 'http://localhost:8000/api'
+})
 
 ReactDOM.render(
-    <Provider store={store}>
+    <ApolloProvider client={client}>
         <App />
-    </Provider>,
+    </ApolloProvider>,
     document.getElementById('root')
 )
 
