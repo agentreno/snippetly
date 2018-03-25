bfbeb51237b8e88212fdc62895b3b2a6b3406ccf
Karl <karl.turrell@buto.tv>
2018-03-25 08:31:16 +0100

    Create store and Provider wrapper at entrypoint

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import { createLogger } from 'redux-logger'
+import thunkMiddleware from 'redux-thunk'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import registerServiceWorker from './registerServiceWorker'
+import './index.css'
+import App from './components/App/App'
+import rootReducer from './reducers'
+import { fetchSnippets } from './actions'
+
+// Create store and fetch data immediately
+const loggerMiddleware = createLogger()
+let store = createStore(
+    rootReducer,
+    applyMiddleware(
+        loggerMiddleware,
+        thunkMiddleware
+    )
+)
+store.dispatch(fetchSnippets())
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+)
+
+registerServiceWorker()
