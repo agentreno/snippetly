50ff1dbc8cafb5ba02a87d654132fde6e3f77cfd
Karl <karl.turrell@buto.tv>
2018-04-26 09:58:12 +0100

    Use Carousel3D component to display snippets

--- client/src/components/SnippetList/SnippetList.js
+++ client/src/components/SnippetList/SnippetList.js
@@ -1,15 +1,22 @@
 import React from 'react'
+import Carousel3D from 'react-3d-carousel'
 import Snippet from '../Snippet/Snippet'
 import './SnippetList.css'
 
 const SnippetList = ({ snippets }) => (
     <div className="SnippetList">
-        {snippets.map(snippet => (
-            <Snippet
-                key={snippet._id}
-                {...snippet}
-            />
-        ))}
+        <Carousel3D
+            panelWidth={600}
+            panelHeight={500}
+            panelRenderers={
+                snippets.map(snippet => () => (
+                    <Snippet
+                        key={snippet._id}
+                        {...snippet}
+                    />
+                ))
+            }
+        />
     </div>
 )
 
