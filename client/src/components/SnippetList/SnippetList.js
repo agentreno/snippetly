53bfa3fb52692a60e2c9aa146d1c7d8498a0bff1
Karl <karl.turrell@buto.tv>
2018-04-28 20:16:54 +0100

    Convert SnippetList to a class component

--- client/src/components/SnippetList/SnippetList.js
+++ client/src/components/SnippetList/SnippetList.js
@@ -3,21 +3,25 @@ import Carousel3D from 'react-3d-carousel'
 import Snippet from '../Snippet/Snippet'
 import './SnippetList.css'
 
-const SnippetList = ({ snippets }) => (
-    <div className="SnippetList">
-        <Carousel3D
-            panelWidth={600}
-            panelHeight={500}
-            panelRenderers={
-                snippets.map(snippet => () => (
-                    <Snippet
-                        key={snippet._id}
-                        {...snippet}
-                    />
-                ))
-            }
-        />
-    </div>
-)
+class SnippetList extends React.Component {
+    render() {
+        return (
+            <div className="SnippetList">
+                <Carousel3D
+                    panelWidth={600}
+                    panelHeight={500}
+                    panelRenderers={
+                        this.props.snippets.map(snippet => () => (
+                            <Snippet
+                                key={snippet._id}
+                                {...snippet}
+                            />
+                        ))
+                    }
+                />
+            </div>
+        )
+    }
+}
 
 export default SnippetList
